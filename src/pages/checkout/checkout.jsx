3a25fb4580362c8f0a5dcb3ce4d1ed2f9ecd3de6
croissant <wathyic@gmail.com>
2021-10-05 13:58:12 +0800

    add increase and decrease item feature to checkout page

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -9,7 +9,6 @@ import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selector
 import "./checkout.scss";
 
 const checkOutPage = (props) => {
-    console.log(props);
     return (
         <div className = "checkout-page" >
             <div className = "checkout-header" >
